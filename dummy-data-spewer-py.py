e5d0d695982c586a5d24a5badbbb44391d2e4bf8
Jacek-Kapral <jacek.kapral@proton.me>
2026-02-01 18:21:09 +0100

    Corrected functions, variables and other cosmetic names

--- dummy-data-spewer-py.py
+++ dummy-data-spewer-py.py
@@ -3,45 +3,45 @@ import requests
 import json
 import random
 
-SENSOR_ID = "STACJA_PZ_001"  
-SERVER_URL = "http://twoj-serwer.com/api/dane"
+SENSOR_ID = "STATION_PZ_001"  
+SERVER_URL = "http://dummy-server-address.com/api/data"
 INTERVAL = 30  
 TIMEOUT = 5  
 
-def pobierz_dane_z_czujnikow():
+def get_sensor_data():
     """
-    Symulacja odczytu pełnego zestawu parametrów.
+    Full data set simulation.
     """
     return {
         "sensor_id": SENSOR_ID,
         "timestamp": time.strftime("%Y-%m-%d %H:%M:%S"),
-        "smog_pm25": round(random.uniform(5, 35), 2),
-        "smog_pm10": round(random.uniform(10, 50), 2),
-        "wilgotnosc": round(random.uniform(40, 60), 1),
+        "pm25": round(random.uniform(5, 35), 2),
+        "pm10": round(random.uniform(10, 50), 2),
+        "humidity": round(random.uniform(40, 60), 1),
         "co2": random.randint(400, 1000),
-        "cisnienie": round(random.uniform(980, 1030), 1),
-        "temperatura": round(random.uniform(15, 22), 1)
+        "pressure": round(random.uniform(980, 1030), 1),
+        "temperature": round(random.uniform(15, 22), 1)
     }
 
 def main():
-    print(f"--- URUCHOMIONO MONITORING ({SENSOR_ID}) ---")
-    print(f"Interwał: {INTERVAL}s | URL: {SERVER_URL}\n")
+    print(f"--- MONITORING STARTED ({SENSOR_ID}) ---")
+    print(f"Time interval: {INTERVAL}s | URL: {SERVER_URL}\n")
 
     while True:
-        dane = pobierz_dane_z_czujnikow()
+        dane = get_sensor_data()
         
         try:
             odpowiedz = requests.post(SERVER_URL, json=dane, timeout=TIMEOUT)
             odpowiedz.raise_for_status()
             
-            print(f"[{dane['timestamp']}] Połączenie OK. Dane wysłane.")
+            print(f"[{dane['timestamp']}] Connection OK. Data sent.")
 
         except (requests.exceptions.RequestException, Exception) as e:
             # Server does not respond, dump info to console
             print(f"\n" + "_"*40)
-            print(f"[{dane['timestamp']}] BŁĄD POŁĄCZENIA!")
-            print(f"Komunikat: {e}")
-            print("DANE LOKALNE (JSON):")
+            print(f"[{dane['timestamp']}] CONNECTION ERROR!")
+            print(f"Error Message: {e}")
+            print("LOCAL DATA (JSON):")
             print(json.dumps(dane, indent=4, ensure_ascii=False))
             print("_"*40 + "\n")
 
